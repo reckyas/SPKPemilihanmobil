4deefd5fb008444e2452086ec7ab9d12121983e8
reckyas <reckyas243@gmail.com>
2023-02-01 02:46:05 +0700

    Tambah format rupiah di input harga tambah mobil

--- resources/views/components/input-text.blade.php
+++ resources/views/components/input-text.blade.php
@@ -2,6 +2,7 @@
     <label for="{{ $name }}">
         <span class="text-slate-700 font-semibold">{{ $label }}</span>
         <input type="{{ $type }}"
+            id="{{ $name }}"
             class="px-3 py-2 border-2 border-slate-500 mt-1 rounded-lg outline-none focus:ring-2 focus:ring-blue-400 text-slate-700 font-semibold w-full disabled:bg-slate-200 disabled:text-slate-500 disabled:cursor-not-allowed"
             name="{{ $name }}" id="{{ $name }}" placeholder="{{ $placeholder }}"
             @if ($required) required @endif
